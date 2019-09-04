da1cbda58691aac7ea0bfbdf9653c75a47349d13
vinobc <abvp3121@gmail.com>
2019-09-04 18:47:06 +0530

    04 loop: enumerate

--- for.py
+++ for.py
@@ -1,7 +1,9 @@
 colors = ("red", "green", "blue", "pink", "orange")
 
 for color in colors:
+# for i,color in enumerate(colors):
 # for i in range(10):
     # if color == "red": continue
     # if color == "blue": break
+    # print(i,color)
     print(color)
