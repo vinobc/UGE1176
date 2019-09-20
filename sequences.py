310110b195bc0e2d8ea750ef144e2e4dc2ef86f9
vinobc <abvp3121@gmail.com>
2019-09-20 10:29:51 +0530

    02 types updated

--- sequences.py
+++ sequences.py
@@ -3,6 +3,15 @@
 # for i in a:
 #     print(i)
 
+# a = [1,2,3,4]
+# b = []
+# for i in a:
+#     if i % 2 == 0:
+#         b.append(i**2 + 10)
+# print(b)
+
+# print([i**2 +10 for i in a if i % 2 ==0])
+
 # b = (10,20,30.4,"a", [1,"two"])
 # b2 = (10,20,30.4,"a", [1,"two"])
 # print(type(b[4]))
@@ -12,12 +21,12 @@
 
 # print(type(b))
 # print(type(b2))
-# print(id(b[1]))
-# print(id(b2[1]))
+# print(id(b))
+# print(id(b2))
 # if b is b2:
-#     print("b and b2 are same objects")
+    # print("b and b2 are same objects")
 # else:
-#     print("b and b2 are not same objects")
+    # print("b and b2 are not same objects")
 
 # if isinstance(b, tuple):
 #     print("b is a tuple")
@@ -25,6 +34,7 @@
 #     print("b is a list")
 # else:
 #     print("b is not a list or a tuple")
+    
 # if type(b) == tuple:
 #     print("b is a tuple")
 # elif type(b) == list:
--- strings.py
+++ strings.py
@@ -2,7 +2,14 @@
 def main():
     a="hello".upper()
     print("a is {0:>10} {1:>10}".format(a, 100))
+
     print(f"a is {a}")
+
+    print(a + " Python " * 3)
+
+    print(a[::-1])
+
+
     
 if __name__ == "__main__":
     main()
