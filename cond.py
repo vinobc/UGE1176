4704e83c1953dbed65cee35687ccce742e0412b0
vinobc <abvp3121@gmail.com>
2019-09-20 10:33:53 +0530

    03 conditionals updated

--- cond.py
+++ cond.py
@@ -1,5 +1,5 @@
 def main():
-    a,b = 20,10
+    # a,b = 20,10
 
     # if a < b:
     #     print("a is less than b")
@@ -8,8 +8,18 @@ def main():
     # else:
     #     print("a is equal to b")
 
-    res = "a is less than b" if (a < b) else "a is greater than or equal to b"
-    print(res)
+    # res = "a is less than b" if (a < b) else "a is greater than or equal to b"
+    # print(res)
+
+    # i = int(input())
+    # if i >= 5:
+    #     print("movie")
+    # elif 1 < i < 5:
+    #     print("shopping")
+    # else:
+    #     print("cricket")
+
+    # print("movie" if i >= 15 else "shopping" if 1 < i < 5 else "cricket")
 
 if __name__ == "__main__":
     main()
